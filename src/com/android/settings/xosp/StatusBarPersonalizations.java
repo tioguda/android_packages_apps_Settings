a067e922dc1a9d702d38eb10e257eba4727eddfd
Janson Kang <temasek71@gmail.com>
2015-09-04 18:58:50 +0800

    Settings: Make all 14 font variants available to Statusbar Clock (2/2)
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- src/com/android/settings/xosp/StatusBarPersonalizations.java
+++ src/com/android/settings/xosp/StatusBarPersonalizations.java
@@ -128,7 +128,7 @@ public class StatusBarPersonalizations extends SettingsPreferenceFragment
         mFontStyle.setOnPreferenceChangeListener(this);
         mFontStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.STATUSBAR_CLOCK_FONT_STYLE,
-                4)));
+                0)));
         mFontStyle.setSummary(mFontStyle.getEntry());
     }
     
