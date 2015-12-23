3306e4cf8e4f17dc7baa837e27190fbb0f7d99f5
Calin <nilac8991@gmail.com>
2015-12-23 19:19:56 +0100

    Fix derps
    Signed-off-by: Calin <nilac8991@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 	android-support-v4 \
 	android-support-v13 \
 	jsr305 \
-	org.cyanogenmod.platform.internal
+	org.cyanogenmod.platform.sdk
 
 LOCAL_MODULE_TAGS := optional
 
